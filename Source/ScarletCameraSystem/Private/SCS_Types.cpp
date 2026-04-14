08616ebb60962c0530c324f4633545d8dcb8cc62
K <Akarfire@gmail.com>
2026-04-14 19:04:12 +0300

    [DEV] Slight transform resolution tweaks

--- Source/ScarletCameraSystem/Private/SCS_Types.cpp
+++ Source/ScarletCameraSystem/Private/SCS_Types.cpp
@@ -1,71 +1,56 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "SCS_Types.h"
 
 
 // Calculates location of the origin point of the boom arm
 FVector FSCS_CameraState::ResolveLocation(AActor* PlayerActor) const
 {
-    FVector Result;
-
 	switch (CameraLocation.LocationType)
 	{
 
 	case ESCS_TransformType::PlayerAttachment:
 		if (PlayerActor)
-			Result = PlayerActor->GetActorLocation() + PlayerActor->GetActorRotation().RotateVector(CameraLocation.Location);
+			return PlayerActor->GetActorLocation() + PlayerActor->GetActorRotation().RotateVector(CameraLocation.Location);
 		else
-			Result = CameraLocation.Location;
-		break;
+			return FVector::ZeroVector;
 
 	case ESCS_TransformType::ActorAttachment:
 		if (CameraLocation.AttachmentActor)
-			Result = CameraLocation.AttachmentActor->GetActorLocation() + CameraLocation.AttachmentActor->GetActorRotation().RotateVector(CameraLocation.Location);
+			return CameraLocation.AttachmentActor->GetActorLocation() + CameraLocation.AttachmentActor->GetActorRotation().RotateVector(CameraLocation.Location);
 		else
-			Result = CameraLocation.Location;
-		break;
+			return FVector::ZeroVector;
 
 	case ESCS_TransformType::World:
-		Result = CameraLocation.Location;
-		break;
+		return CameraLocation.Location;
 
 	default:
-		break;
+		return FVector::ZeroVector;
 	}
-
-    return Result;
 }
 
 // Calculates rotation of the boom arm
 FRotator FSCS_CameraState::ResolveRotation(const FSCS_CameraRotation& Rotation, AActor* PlayerActor) const
 {
-	FRotator Result;
-
 	switch (Rotation.RotationType)
 	{
 
 	case ESCS_TransformType::PlayerAttachment:
 		if (PlayerActor)
-			Result = PlayerActor->GetActorRotation() + Rotation.Rotation;
+			return PlayerActor->GetActorRotation() + Rotation.Rotation;
 		else
-			Result = Rotation.Rotation;
-		break;
+			return FRotator::ZeroRotator;
 
 	case ESCS_TransformType::ActorAttachment:
 		if (Rotation.AttachmentActor)
-			Result = Rotation.AttachmentActor->GetActorRotation() + Rotation.Rotation;
+			return Rotation.AttachmentActor->GetActorRotation() + Rotation.Rotation;
 		else
-			Result = Rotation.Rotation;
-		break;
+			return FRotator::ZeroRotator;
 
 	case ESCS_TransformType::World:
-		Result = Rotation.Rotation;
-		break;
+		return Rotation.Rotation;
 
 	default:
-		break;
+		return FRotator::ZeroRotator;
 	}
-
-	return Result;
 }
